48fcc09d81b918c9dd021410eb77bb28a73acd5d
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-04-26 15:06:03 -0600

    getting things connected to mysql

--- schema.sql
+++ schema.sql
@@ -4,21 +4,31 @@ CREATE DATABASE sykkel_DB;
 
 USE sykkel_DB;
 
-CREATE TABLE products
+CREATE TABLE road_bikes
 (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     item VARCHAR(100) NULL,
     price INT NULL,
     quantity INT NULL
 );
-    ALTER TABLE products AUTO_INCREMENT = 201;
+    ALTER TABLE road_bikes AUTO_INCREMENT = 201;
 
-    -- INSERT INTO products(item, price, quantity)
-    -- VALUES('2019 Pinarello Bolide TT', 14,499, 10),
-    -- ('2019 Kaws Trek Madone', 160,000, 1),
-    -- ('2019 BMC Timemachine ROAD 01', 11,000, 2),
-    -- ();
+    CREATE TABLE mountain_bikes
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    item VARCHAR(100) NULL,
+    price INT NULL,
+    quantity INT NULL
+);
+    ALTER TABLE mountain_bikes AUTO_INCREMENT = 301;
 
-    -- Not sure if it's better to make several .csv files and import them into respective tables, 
-    -- or use INSERT INTO
-    -- Need to think about deducting from inventory and adding $$ to cart
+    CREATE TABLE parts
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    item VARCHAR(100) NULL,
+    price INT NULL,
+    quantity INT NULL
+);
+    ALTER TABLE parts AUTO_INCREMENT = 401;
+
+
--- sykkel.js
+++ sykkel.js
@@ -1,7 +1,7 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
@@ -13,5 +13,50 @@ connection.connect(function (err) {
     start();
 });
 
-// Re-Create a new Git repository called (sykkel), and start again. Then delete climbazon
 // Make an exit choice in the switch case statement in inquirer
+// Not sure if it 's better to make several .csv files and import them into respective tables, 
+// or use INSERT INTO
+// Need to think about deducting from inventory and adding $$ to cart
+
+function start() {
+    console.log('connected to DB');
+    inquirer.prompt({
+        name: 'items',
+        type: 'list',
+        message: 'What are you shopping for?',
+        choices: [
+            'Road Bikes',
+            'Mountain Bikes',
+            'Parts',
+            'Checkout',
+            'EXIT'
+        ]
+    }).then(function (answer) {
+        switch (answer.items) {
+            case 'Road Bikes':
+                roadBikes();
+                break;
+            case 'Mountain Bikes':
+                mountainBikes();
+                break;
+            case 'Parts':
+                parts();
+                break;
+            case 'Checkout':
+                checkout();
+                break;
+            case 'EXIT':
+                exit();
+        }
+    });
+}
+
+// let roadBikes = function () {
+//     let query = 'SELECT item, price FROM road_bikes WHERE ?';
+//     connection.query(query);
+//     for (let i = 0; i < )
+// }
+
+let exit = function () {
+    connection.end();
+}
